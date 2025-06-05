dbbb2b90627c99fae50c71af1335283eded269ed
Husainbasha <nannebaigari02@gmail.com>
2025-06-05 07:11:41 +0530

    add new files

---
+++ SeedWeight/SeedsWeight.java
@@ -0,0 +1,18 @@
+import java .util.Scanner;
+public class Main
+{
+	public static void main(String[] args) {
+	Scanner scanner=new Scanner(System.in);
+        System.out.println("enter the prise per 100kg:");
+	double prise = scanner.nextDouble();
+	System.out.println("enter the weight you get from (AV) :");
+        double weight = scanner.nextDouble();
+        double Actualweight=(weight/100)*3;
+        double Realweight=weight-Actualweight;
+        Double Amount=Realweight*prise/100;
+        System.out.println("Actual weight:"+Actualweight);
+        System.out.println("original weight:"+Realweight);
+        System.out.println("Actual Amout you get:"+Amount);
+	}
+      
+}
